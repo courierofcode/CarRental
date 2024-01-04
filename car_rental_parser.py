9a02438e56a4966c1970a6c87eec820ef6b6125a
Emmanuel <notifyemmanuel@gmail.com>
2024-01-04 10:59:19 -0600

    Car rental parse plan

---
+++ car_rental_parser.py
@@ -0,0 +1,31 @@
+# Validate command line arguments
+
+# Validate file exists
+
+# Validate file name format
+
+# Validate file syntax (JSON file for best practice)
+
+# Parse Input JSON file
+"""
+    events = [{
+        "id": 
+        "type":
+        "timestamp":
+        "comments":
+        }]
+    List of dictionaries
+"""
+
+# Write Output JSON file
+"""
+    records = [{
+        "session_id": 
+        "session_start":
+        "session_end":
+        "session_duration":
+        "returned_late": 
+        "car_damaged": 
+        }]
+    List of dictionaries
+"""
