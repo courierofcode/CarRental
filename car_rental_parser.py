fd9a37def077cafaedb409430de3f8d83e76b7a8
Emmanuel <notifyemmanuel@gmail.com>
2024-01-04 11:29:47 -0600

    Input file validation

--- car_rental_parser.py
+++ car_rental_parser.py
@@ -1,31 +1,64 @@
-# Validate command line arguments
+import os
+import sys
+import json
+import datetime
 
-# Validate file exists
+def validate_file(events_file):
+    # Validate file location
+    if os.path.isfile(events_file) == False:
+        print("Error: File does not exist")
+        exit()
+    # Validate file name format
+    if events_file.endswith(".json") == False:
+        print("Error: Input file format - <events>.json")
+        exit()
 
-# Validate file name format
+    else:
+        # Validate file syntax (JSON file for best practice)
+        with open(events_file, 'r') as events_data:
+            try: 
+                events = json.load(events_data)
+            except json.JSONDecodeError as e:
+                print(f"Error: JSON decoding error {e}")
+                exit()
+        return events
+    
 
-# Validate file syntax (JSON file for best practice)
+def main():
+    # Validate command line arguments
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Error: Wrong number of arguments")
+        print("Usage: py car_rental_parser.py <events>.json")
+        exit()
 
-# Parse Input JSON file
-"""
-    events = [{
-        "id": 
-        "type":
-        "timestamp":
-        "comments":
-        }]
-    List of dictionaries
-"""
+    if len(sys.argv) == 2:
+        # Validate input file
+        events_file = str(sys.argv[1])
+        events = validate_file(events_file)
 
-# Write Output JSON file
-"""
-    records = [{
-        "session_id": 
-        "session_start":
-        "session_end":
-        "session_duration":
-        "returned_late": 
-        "car_damaged": 
-        }]
-    List of dictionaries
-"""
+        # Parse Input JSON file
+        """
+            events = [{
+                "id": 
+                "type":
+                "timestamp":
+                "comments":
+                }]
+            List of dictionaries
+        """
+
+        # Write Output JSON file
+        """
+            records = [{
+                "session_id": 
+                "session_start":
+                "session_end":
+                "session_duration":
+                "returned_late": 
+                "car_damaged": 
+                }]
+            List of dictionaries
+        """
+
+if __name__ == '__main__':
+    main()
